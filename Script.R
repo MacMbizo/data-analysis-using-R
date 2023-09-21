84814ee299ea0f65280438bb9cf012f693c907ed
Mac Mbizo <macmbizo@gmail.com>
2023-09-21 20:03:03 +0200

    Viewing the 2 datasets.

--- Script.R
+++ Script.R
@@ -2,3 +2,9 @@ tuesdata <- tidytuesdayR::tt_load('2021-09-14')
 tuesdata <- tidytuesdayR::tt_load(2021, week = 38)
 
 billboard <- tuesdata$billboard
+
+beats <- tuesdata$audio_features
+View(billboard)
+View(beats)
+
+
